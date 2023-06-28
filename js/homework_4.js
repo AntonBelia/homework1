4bdbae9ef9506a652009592580f00713f776465b
AntonBelia <134911799+AntonBelia@users.noreply.github.com>
2023-06-28 18:54:21 +0300

    add task1

---
+++ js/homework_4.js
@@ -0,0 +1,4 @@
+let blockFirestLine = document.querySelector('section:first-line');
+let blockTwoHeader = document.querySelector('.blockTwo__Header');
+let blockTwoList = document.querySelector('ul li:nth-child(5)');
+let footerLevelCurrent = document.querySelector('#footerLevelCurrent');
