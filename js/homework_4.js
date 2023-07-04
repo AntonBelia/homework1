bdce20176818b2e85e1a705b0f828584740eb1bb
AntonBelia <134911799+AntonBelia@users.noreply.github.com>
2023-07-04 19:30:14 +0300

    corect

--- js/homework_4.js
+++ js/homework_4.js
@@ -1,53 +1,66 @@
 //Task 1
 
-// let blockFirestLine = document.querySelector('section:first-line');
-// let blockTwoHeader = document.querySelector('.blockTwo__Header');
+// let blockFirestLine = document.getElementsByClassName('firstSection');
+// let blockTwoHeader = document.getElementById('headerTwo');
 // let blockTwoList = document.querySelector('ul li:nth-child(5)');
-// let footerLevelCurrent = document.querySelector('#footerLevelCurrent');
-
+// let footerLevelCurrent = document.querySelectorAll('hatredLevelBlock');
 
 //Task 2
-let flowData = document.getElementById('flowData');
-let toggleButton = document.getElementById('button');
-let isButton = localStorage.getItem('isButton');
-let lastToggleTime = localStorage.getItem('lastToggleTime');
-const bgColorDark = 'rgb(65, 65, 65)';
-const bgColorLigth = 'rgb(225, 225, 225)';
+let outputLastDate = document.getElementById("outputLastDate");
+let toggleButton = document.getElementById("button");
+let bgColorTheme = localStorage.getItem("bgColorTheme");
+let lastToggleTime = localStorage.getItem("lastToggleTime");
 
-function updateButtonState() {
-	if (isButton === 'true' ) {
-		toggleButton.textContent = 'Turn off' ;
-		document.body.style.background = bgColorLigth;
-		flowData.innerHTML = `Last turn on:` + lastToggleTime;
-	} else {
-		toggleButton.textContent = 'Turn on' ;
-		document.body.style.background = bgColorDark;
-		flowData.innerHTML = `Last turn off:` + lastToggleTime;
-	}
+function getFormattedDate() {
+  let newDate = new Date();
+  let day = ("0" + newDate.getDate()).slice(-2);
+  let month = ("0" + (newDate.getMonth() + 1)).slice(-2);
+  let year = newDate.getFullYear();
+  let hours = ("0" + newDate.getHours()).slice(-2);
+  let minutes = ("0" + newDate.getMinutes()).slice(-2);
+  let seconds = ("0" + newDate.getSeconds()).slice(-2);
+  return `${day}-${month}-${year} ${hours}:${minutes}:${seconds}`;
 }
 
-if (isButton !== null && lastToggleTime !== null) {
-    updateButtonState();
-} else {
-    isButton = 'true';
-    lastToggleTime = '';
-    localStorage.setItem('isButton', isButton);
-    localStorage.setItem('lastToggleTime', lastToggleTime);
+console.log(getFormattedDate());
+
+if (!bgColorTheme || !lastToggleTime) {
+  lastToggleTime = "";
+  bgColorTheme = "light";
+  localStorage.setItem("lastToggleTime", lastToggleTime);
+  localStorage.setItem("bgColorTheme", bgColorTheme);
 }
 
-function toggleButtonClick() {
-    if (isButton === 'true') {
-        isButton = 'false';
-        lastToggleTime = new Date().toLocaleString();
+document.body.classList.add(bgColorTheme);
+
+toggleButton.textContent = bgColorTheme === "light" ? "Turn off" : "Turn on";
+
+if (bgColorTheme === "light") {
+    outputLastDate.textContent = "Last turn off: " + lastToggleTime;
+  } else {
+    outputLastDate.textContent = "Last turn on: " + lastToggleTime;
+  }
+
+  toggleButton.addEventListener("click", function () {
+    if (document.body.classList.contains("light")) {
+      document.body.classList.remove("light");
+      document.body.classList.add("dark");
+      toggleButton.textContent = "Turn on";
+      outputLastDate.textContent = "Last turn off: " + getFormattedDate();
+
+      lastToggleTime = getFormattedDate();
+      bgColorTheme = "dark";
+      localStorage.setItem("lastToggleTime", lastToggleTime);
+      localStorage.setItem("bgColorTheme", bgColorTheme);
     } else {
-        isButton = 'true';
-        lastToggleTime = new Date().toLocaleString();
+      document.body.classList.remove("dark");
+      document.body.classList.add("light");
+      toggleButton.textContent = "Turn off";
+      outputLastDate.textContent = "Last turn on: " + getFormattedDate();
+  
+      lastToggleTime = getFormattedDate();
+      bgColorTheme = "light";
+      localStorage.setItem("lastToggleTime", lastToggleTime);
+      localStorage.setItem("bgColorTheme", bgColorTheme);
     }
-
-    localStorage.setItem('isButton', isButton);
-    localStorage.setItem('lastToggleTime', lastToggleTime);
-
-    updateButtonState();
-}
-
-toggleButton.addEventListener('click', toggleButtonClick);
+  });
